457d91da929d37f8019e3c9f43be32ab52ebf743
Mahesh Kale <maheshkale459@gmail.com>
2024-01-02 23:37:55 +0530

    Adde Todos,Initial Commit.

---
+++ workflow.py
@@ -0,0 +1,9 @@
+print("Hello World!!")
+
+# Todo: download data for stocks from yfinance into csv
+# Todo: turn csv into database
+# Todo: Create tables in postgress for each stock
+# Todo: primary key will be time epoch for stock value
+# Todo: Add Support For Extension
+# Todo: Add Indicators
+# Todo: Add Support For ML Models
